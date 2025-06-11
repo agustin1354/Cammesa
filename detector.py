83312732d31cb3551a9fd333db054944fd1fc6bf
agustin1354 <agustin1354_2@hotmail.com>
2025-06-11 09:16:37 -0300

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,16 +1,7 @@
 # detector.py
 
 def check_peak(current, yesterday, last_week, threshold_percentage=20):
-    """
-    Devuelve si hay caída significativa en la demanda (menos del umbral respecto a ayer Y semana pasada)
-    
-    :param current: Demanda actual
-    :param yesterday: Demanda de ayer a esta hora
-    :param last_week: Demanda de la semana pasada a esta hora
-    :param threshold_percentage: Porcentaje mínimo de caída para considerarse pico negativo
-    :return: (bool, list) -> (si hay pico, lista de causas)
-    """
-    if None in (current, yesterday, last_week):
+   if None in (current, yesterday, last_week):
         print("❌ No se pueden calcular picos: uno o más valores son None")
         return False, []
 
