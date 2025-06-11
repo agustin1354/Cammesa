1ecb683109bf1171bee1c41b6e0872c7ec8ccc5b
agustin1354 <agustin1354_2@hotmail.com>
2025-06-11 16:46:40 -0300

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,5 +1,9 @@
 # detector.py
 
+print("DEBUG - current:", current)
+print("DEBUG - previous_measurement:", previous_measurement)
+print("DEBUG - diff_previous:", diff_previous)
+
 def check_peak(current, yesterday, last_week, history, threshold_daily=20, threshold_last_measurement=10):
     """
     Devuelve (bool, list) → si hay alerta y causas
