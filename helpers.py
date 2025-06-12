d36de36493f42d1177c6bae57524cb1d43b4092e
agustin1354 <agustin1354_2@hotmail.com>
2025-06-12 17:53:08 -0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 # helpers.py
 
-def generate_alert_html(region_name, region_id, timestamp, current, yesterday, last_week, history, reasons):
+def generate_alert_html(region_name, region_id, timestamp, current, yesterday, last_week, history, , reasons,a_threshold, b_threshold):
     """
     Genera cuerpo HTML del correo según datos de alerta
     """
@@ -16,8 +16,8 @@ def generate_alert_html(region_name, region_id, timestamp, current, yesterday, l
 
         <h2 style="color:#3F51B5;">📌 Configuración actual de alertas:</h2>
         <ul>
-            <li><strong>A)</strong> Demanda actual es <strong>{reasons.A_THRESHOLD}%</strong> menor que la de Ayer y la Semana pasada (Se deben cumplir ambas)</li>
-            <li><strong>B)</strong> Demanda actual es ≥<strong>{reasons.B_THRESHOLD}%</strong> menor que la medición inmediata anterior</li>
+            <li><strong>A)</strong> Demanda actual es <strong>{a_threshold}%</strong> menor que la de Ayer y la Semana pasada (Se deben cumplir ambas)</li>
+            <li><strong>B)</strong> Demanda actual es ≥<strong>{b_threshold}%</strong> menor que la medición inmediata anterior</li>
         </ul>
 
         <h2 style="color:#3F51B5;">📍 Región: {region_name} (ID: {region_id})</h2>
