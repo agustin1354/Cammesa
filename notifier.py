e4b582f6b096594ec11919376b76cfc913677e55
agustin1354 <agustin1354_2@hotmail.com>
2025-06-12 09:52:59 -0300

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -3,22 +3,26 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from config import EMAIL_FROM, EMAIL_PASSWORD, EMAIL_ALERT_TO
 
-def send_email(subject, body):
-    from config import EMAIL_FROM, EMAIL_PASSWORD, EMAIL_ALERT_TO
 
-    msg = MIMEMultipart()
+def send_email(subject, body_html):
+    """
+    Envía un correo con formato HTML
+    """
+    msg = MIMEMultipart("alternative")
     msg['From'] = EMAIL_FROM
     msg['To'] = EMAIL_ALERT_TO
     msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
+
+    # Adjuntar contenido HTML
+    msg.attach(MIMEText(body_html, "html"))
 
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(EMAIL_FROM, EMAIL_PASSWORD)
-        text = msg.as_string()
-        server.sendmail(EMAIL_FROM, EMAIL_ALERT_TO, text)
+        server.sendmail(EMAIL_FROM, EMAIL_ALERT_TO, msg.as_string())
         print("✅ Correo enviado exitosamente.")
     except Exception as e:
         print(f"❌ Error enviando correo: {e}")
