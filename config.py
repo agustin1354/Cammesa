9ec79bb9c5a6c8c0d5c66173369e6ecbaa5fa4a6
agustin1354 <agustin1354_2@hotmail.com>
2025-06-13 09:12:18 -0300

    Update config.py

--- config.py
+++ config.py
@@ -50,8 +50,8 @@ REGIONS = {
 
 import os
 
-THRESHOLD_DAILY = 20     # % caída mínima para disparar alerta vs Ayer y Semana pasada
-THRESHOLD_LAST_MEASUREMENT = 10  # % caída mínima vs medición inmediata anterior
+THRESHOLD_DAILY = 25     # % caída mínima para disparar alerta vs Ayer y Semana pasada
+THRESHOLD_LAST_MEASUREMENT = 20  # % caída mínima vs medición inmediata anterior
 # THRESHOLD_DAILY = float(os.getenv("THRESHOLD_PERCENTAGE", "20"))
 SHOW_THRESHOLD_A = THRESHOLD_DAILY
 SHOW_THRESHOLD_B = THRESHOLD_LAST_MEASUREMENT
